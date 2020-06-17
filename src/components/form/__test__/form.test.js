17aabbc810978e2a0b57fc9aee040515fe30bb33
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-17 08:53:18 -0500

    wrote test for identifying form text content

--- src/components/form/__test__/form.test.js
+++ src/components/form/__test__/form.test.js
@@ -1,8 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Form from './../form';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from '../form';
 
-// it("renders without crashing", () => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(<form></form>, div)
-// });
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Form></Form>, div)
+});
+
+it("renders form correctly", () => {
+    const {getByTestId} = render(<Form label="URL: GO!GETPOSTPUTDELETE"></Form>);
+    expect (getByTestId('form')).toHaveTextContent("URL: GO!GETPOSTPUTDELETE");
+})
