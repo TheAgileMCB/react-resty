6e0f43a64256439d2160104323a0772b7efa0396
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-20 14:59:51 -0500

    modified header links, added result

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,8 +12,9 @@ class Header extends React.Component {
           <nav>
             <ul>
               <li><Link to="/">Home</Link></li>
+              <li><Link to="/results">Results</Link></li>
               <li><Link to="/about">About Us</Link></li>
-              <li><Link to="https://en.wikipedia.org/wiki/Representational_state_transfer">Additional Resource</Link></li>
+              <li><a target="_blank" href="https://en.wikipedia.org/wiki/Representational_state_transfer">Additional Resource</a></li>
             </ul>
           </nav>
           
