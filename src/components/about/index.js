f8809525bf25542515bfbae741e20e1626d39aa1
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-20 14:34:19 -0500

    made dummy about page

---
+++ src/components/about/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>About Us</h1>
+                <p>Tattooed palo santo tofu, poutine YOLO subway tile freegan knausgaard forage man braid hot chicken readymade adaptogen. Tilde tacos kitsch whatever marfa. Cornhole ennui man bun, butcher pitchfork semiotics pour-over direct trade mumblecore food truck. Hell of small batch four loko waistcoat, 90's street art iceland pinterest hexagon direct trade mlkshk 8-bit raw denim.
+<br /><br />
+Mlkshk affogato master cleanse distillery tilde, leggings cliche woke fingerstache. Knausgaard portland everyday carry brunch try-hard, vaporware taiyaki bicycle rights biodiesel kickstarter 90's fam iceland. Bitters master cleanse four loko bushwick keffiyeh yr tote bag. Pok pok poke vice, art party mumblecore humblebrag aesthetic kombucha master cleanse.
+<br /><br />
+Poke tote bag pop-up, pabst unicorn biodiesel art party portland tattooed next level ennui fanny pack locavore ethical sriracha. Keytar semiotics subway tile kitsch vexillologist portland adaptogen bushwick whatever. Edison bulb cloud bread kogi cardigan biodiesel prism. Tote bag DIY YOLO cronut, aesthetic next level church-key flexitarian jean shorts gluten-free.
+<br /><br />
+Skateboard food truck poke roof party. Meh tattooed forage, ugh PBR&B blog palo santo tilde deep v paleo you probably haven't heard of them YOLO lomo pok pok. Echo park jianbing food truck, etsy mustache fixie gochujang bicycle rights tote bag single-origin coffee schlitz whatever. Put a bird on it selfies typewriter man braid trust fund pork belly banh mi. Hella hot chicken subway tile try-hard 90's, vice snackwave master cleanse la croix leggings fixie mlkshk letterpress artisan kickstarter. Literally health goth taiyaki disrupt chillwave letterpress migas pitchfork, tumeric selvage man braid raw denim cray DIY.
+<br /><br />
+Lumbersexual ugh unicorn marfa, before they sold out try-hard humblebrag pour-over. Messenger bag craft beer sustainable church-key cold-pressed, selfies celiac aesthetic. Wolf polaroid disrupt forage truffaut master cleanse literally mustache. Hella slow-carb etsy before they sold out vegan. Biodiesel next level cornhole mustache, tote bag synth sartorial selfies vice fam kitsch wayfarers. Copper mug readymade 3 wolf moon pok pok unicorn mustache. Keffiyeh wolf 3 wolf moon gochujang vice XOXO pabst paleo disrupt.
+<br /><br />
+Dummy text? More like dummy thicc text, amirite?</p>
+            </>
+        )
+    }
+}
+
+export default About;
