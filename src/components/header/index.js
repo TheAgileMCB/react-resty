5d8c217802f73d74b11966bd539d1eac4d8df9a4
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-20 15:13:03 -0500

    added security rel to _blank link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -14,7 +14,7 @@ class Header extends React.Component {
               <li><Link to="/">Home</Link></li>
               <li><Link to="/results">Results</Link></li>
               <li><Link to="/about">About Us</Link></li>
-              <li><a target="_blank" href="https://en.wikipedia.org/wiki/Representational_state_transfer">Additional Resource</a></li>
+              <li><a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Representational_state_transfer">Additional Resource</a></li>
             </ul>
           </nav>
           
