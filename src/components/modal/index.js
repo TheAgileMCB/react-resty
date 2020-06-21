9bb46e4f9f569ab9729a43917013bcd9eab01955
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-21 12:34:09 -0500

    finished modal component

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-class Modal extends React.Component {
-    render() {
-        return (
+const Modal = props => {
+    const { title, onClose, children } = props;
 
-        )
-    }
-
+    return (
+        <div className="overlay">
+            <div className="modal">
+                <div clasName="header">
+                    <span className="title">{title}</span>
+                    <button onClick={onClose}>&times;</button>
+                </div>
+                <div className="content">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Modal;
